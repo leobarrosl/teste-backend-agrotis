66ce02ec49de69cf370121aa7836b5cfcbf1c7de
Leonardo Barros Leite <59918471+leobarrosl@users.noreply.github.com>
2025-04-19 10:28:27 -0300

    Inicialização

---
+++ src/main/java/com/leonardoleie/teste_agrotis/TesteAgrotisApplication.java
@@ -0,0 +1,13 @@
+package com.leonardoleie.teste_agrotis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteAgrotisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteAgrotisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leonardoleie/teste_agrotis/TesteAgrotisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leonardoleie.teste_agrotis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteAgrotisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
