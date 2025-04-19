956b8e252a1ced3c38d25cb96da45637f8323137
Leonardo Barros Leite <59918471+leobarrosl@users.noreply.github.com>
2025-04-19 18:31:24 -0300

    feat: Repositórios com filtros

--- src/main/java/com/leonardoleie/teste_agrotis/repositories/LaboratorioRepository.java
+++ src/main/java/com/leonardoleie/teste_agrotis/repositories/LaboratorioRepository.java
@@ -2,6 +2,7 @@ package com.leonardoleie.teste_agrotis.repositories;
 
 import com.leonardoleie.teste_agrotis.models.Laboratorio;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface LaboratorioRepository extends JpaRepository<Laboratorio, Long> {
+public interface LaboratorioRepository extends JpaRepository<Laboratorio, Long>, JpaSpecificationExecutor<Laboratorio> {
 }
--- src/main/java/com/leonardoleie/teste_agrotis/repositories/PessoaRepository.java
+++ src/main/java/com/leonardoleie/teste_agrotis/repositories/PessoaRepository.java
@@ -2,6 +2,7 @@ package com.leonardoleie.teste_agrotis.repositories;
 
 import com.leonardoleie.teste_agrotis.models.Pessoa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+public interface PessoaRepository extends JpaRepository<Pessoa, Long>, JpaSpecificationExecutor<Pessoa> {
 }
