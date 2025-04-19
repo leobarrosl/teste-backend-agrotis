ca78fa48bfa54909d865ce4a8908dc61fe12fa94
Leonardo Barros Leite <59918471+leobarrosl@users.noreply.github.com>
2025-04-19 14:12:42 -0300

    fea: Criação dos services

---
+++ src/main/java/com/leonardoleie/teste_agrotis/services/LaboratorioService.java
@@ -0,0 +1,42 @@
+package com.leonardoleie.teste_agrotis.services;
+
+import com.leonardoleie.teste_agrotis.exceptions.NotFoundException;
+import com.leonardoleie.teste_agrotis.models.Laboratorio;
+import com.leonardoleie.teste_agrotis.repositories.LaboratorioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class LaboratorioService {
+
+    private final LaboratorioRepository laboratorioRepository;
+
+    public Laboratorio findById(Long id) {
+        return laboratorioRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Laboratório não encontrado"));
+    }
+
+    public List<Laboratorio> findAll() {
+        return laboratorioRepository.findAll();
+    }
+
+    public Page<Laboratorio> findAllPageable(Pageable pageable) {
+        return laboratorioRepository.findAll(pageable);
+    }
+
+    public Laboratorio save(Laboratorio laboratorio) {
+        if (!laboratorio.isValid()) {
+            throw new IllegalArgumentException("Laboratório inválido");
+        }
+        return laboratorioRepository.save(laboratorio);
+    }
+
+    public void deleteById(Long id) {
+        laboratorioRepository.delete(findById(id));
+    }
+}
---
+++ src/main/java/com/leonardoleie/teste_agrotis/services/PessoaService.java
@@ -0,0 +1,42 @@
+package com.leonardoleie.teste_agrotis.services;
+
+import com.leonardoleie.teste_agrotis.exceptions.NotFoundException;
+import com.leonardoleie.teste_agrotis.models.Pessoa;
+import com.leonardoleie.teste_agrotis.repositories.PessoaRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class PessoaService {
+
+    private final PessoaRepository pessoaRepository;
+
+    public Pessoa findById(Long id) {
+        return pessoaRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Pessoa não encontrada"));
+    }
+
+    public List<Pessoa> findAll() {
+        return pessoaRepository.findAll();
+    }
+
+    public Page<Pessoa> findAllPageable(Pageable pageable) {
+        return pessoaRepository.findAll(pageable);
+    }
+
+    public Pessoa save(Pessoa pessoa) {
+        if (!pessoa.isValid()) {
+            throw new IllegalArgumentException("Dados inválidos");
+        }
+        return pessoaRepository.save(pessoa);
+    }
+
+    public void deleteById(Long id) {
+        pessoaRepository.delete(findById(id));
+    }
+}
