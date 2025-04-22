bb4bffc3982a168018b1cb0b193145f97b5191bd
Leonardo Barros Leite <59918471+leobarrosl@users.noreply.github.com>
2025-04-21 22:51:30 -0300

    feat: Ajuste de nome no swagger

--- src/main/java/com/leonardoleie/teste_agrotis/configurations/OpenApiConfig.java
+++ src/main/java/com/leonardoleie/teste_agrotis/configurations/OpenApiConfig.java
@@ -13,11 +13,11 @@ public class OpenApiConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("API de Gerenciamento")
+                        .title("API - Agrotis")
                         .version("1.0")
-                        .description("API para gerenciamento de pessoas, propriedades e laboratórios")
+                        .description("API de resolução do teste da empresa Agrotis")
                         .contact(new Contact()
                                 .name("Leonardo Leite")
-                                .email("seu.email@exemplo.com")));
+                                .email("leonardoleite99@gmail.com")));
     }
 }
